ce9e120bc7ad4a4779761831a490e53ebf3bb08d
Prateek <emaillawrathod@gmail.com>
2020-07-17 12:47:08 +0530

    added home and css

--- src/App.js
+++ src/App.js
@@ -5,75 +5,128 @@ import Music from "./music";
 import Projects from "./projects";
 import People from "./people";
 import { Route, Switch, NavLink } from "react-router-dom";
-import * as fb from "./firebase";
+import fb from "./firebase";
 import "./App.scss";
+import Spinner from "react-bootstrap/Spinner";
 
-function App() {
-  const fbd = fb.init(); // eslint-disable-line
-  return (
-    <div className="mainApp">
-      <div id="navigation">
-        <div>
-          <NavLink
-            activeStyle={{ opacity: "100%" }}
-            className="navLinks"
-            to="/"
-            exact
-          >
-            Home
-          </NavLink>
-        </div>
-        <div>
-          <NavLink
-            activeStyle={{ opacity: "100%" }}
-            className="navLinks"
-            to="/journal"
-          >
-            Journal
-          </NavLink>
-        </div>
-        <div>
-          <NavLink
-            activeStyle={{ opacity: "100%" }}
-            className="navLinks"
-            to="/music"
-          >
-            Music
-          </NavLink>
-        </div>
-        <div>
-          <NavLink
-            activeStyle={{ opacity: "100%" }}
-            className="navLinks"
-            to="/projects"
-          >
-            Projects
-          </NavLink>
-        </div>
-        <div>
-          <NavLink
-            activeStyle={{ opacity: "100%" }}
-            className="navLinks"
-            to="/people"
-          >
-            People
-          </NavLink>
-        </div>
-      </div>
-      <div id="separator"></div>
-      <div>
-        <main>
-          <Switch>
-            <Route path="/" render={() => <Home text="Home" />} exact />
-            <Route path="/journal" render={() => <Journal />} exact />
-            <Route path="/music" render={() => <Music />} exact />
-            <Route path="/projects" render={() => <Projects />} exact />
-            <Route path="/people" render={() => <People />} exact />
-          </Switch>
-        </main>
-      </div>
-    </div>
-  );
+async function getHome(db) {
+  const contentRef = db.collection("home").doc("content");
+  const docey = await contentRef.get();
+  const doc = docey.data();
+  const linksRef = db.collection("home").doc("urls");
+  const docey1 = await linksRef.get();
+  const doc1 = docey1.data();
+  const home = {
+    content: {
+      title: doc.title,
+      para: doc.para,
+    },
+    urls: doc1,
+  };
+  return home;
 }
 
-export default App;
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { home: {} };
+    this.fbd = new fb();
+  }
+  async componentDidMount() {
+    const data = await getHome(this.fbd.getDb());
+    this.setState({
+      home: data,
+    });
+    window.addEventListener("orientationchange", () => {
+      window.location.reload();
+    });
+  }
+  render() {
+    return Object.keys(this.state.home).length ? (
+      window.screen.orientation.type === "landscape-primary" ? (
+        this.renderApp()
+      ) : (
+        <div>
+          <h1>Sorry! Didn't add enough css! Please Rotate.</h1>
+        </div>
+      )
+    ) : (
+      <div id="loadSpinner">
+        <Spinner animation="border" role="status">
+          <span className="sr-only">Loading...</span>
+        </Spinner>
+      </div>
+    );
+  }
+
+  renderApp = () => {
+    return (
+      <div id="mainApp">
+        <div id="navigation">
+          <div>
+            <NavLink
+              activeStyle={{ opacity: "100%" }}
+              className="navLinks"
+              to="/"
+              exact
+            >
+              Home
+            </NavLink>
+          </div>
+          <div>
+            <NavLink
+              activeStyle={{ opacity: "100%" }}
+              className="navLinks"
+              to="/journal"
+            >
+              Journal
+            </NavLink>
+          </div>
+          <div>
+            <NavLink
+              activeStyle={{ opacity: "100%" }}
+              className="navLinks"
+              to="/music"
+            >
+              Music
+            </NavLink>
+          </div>
+          <div>
+            <NavLink
+              activeStyle={{ opacity: "100%" }}
+              className="navLinks"
+              to="/projects"
+            >
+              Projects
+            </NavLink>
+          </div>
+          <div>
+            <NavLink
+              activeStyle={{ opacity: "100%" }}
+              className="navLinks"
+              to="/people"
+            >
+              People
+            </NavLink>
+          </div>
+        </div>
+        <div id="separator"></div>
+        <div>
+          <main>
+            <Switch>
+              <Route
+                path="/"
+                render={() => <Home db={this.db} data={this.state.home} />}
+                exact
+              />
+              <Route path="/journal" render={() => <Journal />} exact />
+              <Route path="/music" render={() => <Music />} exact />
+              <Route path="/projects" render={() => <Projects />} exact />
+              <Route path="/people" render={() => <People />} exact />
+            </Switch>
+          </main>
+        </div>
+      </div>
+    );
+  };
+}
--- src/home.jsx
+++ src/home.jsx
@@ -1,13 +1,35 @@
 import React from "react";
 import "./home.scss";
 
-function Home(props) {
-  return (
-    <div className="Home">
-      <h1>{props.text}</h1>
-    </div>
-  );
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div id="Home">
+        <div id="homeContent">
+          <div id="homeTitle">
+            <span>{this.props.data.content.title}</span>
+          </div>
+          <br />
+          <br />
+          <div id="homePara">
+            <p>{this.props.data.content.para}</p>
+          </div>
+        </div>
+        <div id="homeUrlsBox">
+          {Object.keys(this.props.data.urls).map((key) => (
+            <div>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                className="homeUrls"
+                href={this.props.data.urls[key]}
+              >
+                {key}
+              </a>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
-
-export default Home;
-
