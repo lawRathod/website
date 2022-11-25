b46582d394539b31891731a1811264dae1ac94cf
Prateek Rathod <emaillawrathod@gmail.com>
2022-11-26 00:12:58 +0530

    chore: prep before deploying on ipfs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Hello!! I am Prateek...</h1>
+<p>Building with svelte. Better looking website coming up soon!</p>
