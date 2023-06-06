7f5a1193403d32ec07d1ba4822db7c7f87cf7006
Prateek Rathod <emaillawrathod@gmail.com>
2023-06-07 01:32:22 +0530

    chore: add under construction main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Hello!! I am Prateek...</h1>
-<p>Building with svelte. Better looking website coming up soon!</p>
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<div class="container">
+	<p>Under Construction! 🏗️</p>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		justify-content: center;
+
+    font-size: 3rem;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	}
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,23 @@
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   // preprocess: preprocess(),
+  //
+  // SSG adaptor
+  // kit: {
+  //   adapter: adapter({
+  //     fallback: 'index.html'
+  //   })
+  // }
+  //
+  // Vercel adaptor
   kit: {
     adapter: adapter({
-      fallback: 'index.html'
+      // see the 'Deployment configuration' section below
     })
   }
 };
