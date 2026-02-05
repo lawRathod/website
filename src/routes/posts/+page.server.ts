25f5c0f2a9a4049feebbc3b62d2e4eba3fd3fff2
Prateek Rathod <emaillawrathod@gmail.com>
2026-02-06 00:16:32 +0100

    clean all

--- src/routes/posts/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import { getPostsMetadata } from '$lib/posts.js';
-import type { PageServerLoad } from './$types';
-
-export const load: PageServerLoad = async () => {
-	const posts = await getPostsMetadata();
-	return {
-		posts
-	};
-};
